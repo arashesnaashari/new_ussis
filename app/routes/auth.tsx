2cde2d0d1245cdd582405e9137b1ead187d12418
arashesnaashari <arash.esnaaashari@gmail.com>
2024-01-25 17:36:15 +0330

    root_update

---
+++ app/routes/auth.tsx
@@ -0,0 +1,12 @@
+import type { MetaFunction } from "@remix-run/node";
+import { AudioRecorder } from "react-audio-voice-recorder";
+import { Box } from "@chakra-ui/react";
+import { App } from "../components/SignUpWithQuote/App";
+export default function Index() {
+  return (
+    <>
+      {/* <Box>SSS</Box> */}
+      <App />
+    </>
+  );
+}
